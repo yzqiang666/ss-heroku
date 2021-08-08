223ff4985f09bf2d495f03278d6f3f1ea0219a82
yzqiang666 <yzqiang6@gmail.com>
2021-08-08 19:55:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -105,7 +105,7 @@ else
   echo "${ss}" | tr -d '\n' > /wwwroot/${QR_Path}/index.html
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/${QR_Path}/v2.png
 fi
-
+gost -L=ss+ws://${ENCRYPT}:${PASSWORD}@:2334 &
 ss-server -c /etc/shadowsocks-libev/config.json &
 rm -rf /etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
