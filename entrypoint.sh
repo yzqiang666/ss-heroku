2ebad4cc40f2e543e2a8f086f7408ea07d78e36e
yzqiang666 <yzqiang6@gmail.com>
2021-08-09 22:30:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-[ ! "${ENTRYPOINT_URL}" == "" ] && wget --no-check-certificate  -O entrypoint.tmp "$ENTRYPOINT_URL"
-if [ -s entrypoint.tmp ] && [ ! "`grep \"#!/bin/bash\" entrypoint.tmp`" == "" ] ; then
+[ ! "${ENTRYPOINT_URL}" == "" ] && wget --no-check-certificate  -O /entrypoint.tmp "$ENTRYPOINT_URL"
+if [ -s /entrypoint.tmp ] && [ ! "`grep \"#!/bin/bash\" /entrypoint.tmp`" == "" ] ; then
    echo "Download from url ${ENTRYPOINT_URL} file success." 
-   mv entrypoint.tmp /entrypoint0.sh 
+   mv /entrypoint.tmp /entrypoint0.sh 
    echo "################# USE NEW SHELL ##############" >>/entrypoint0.sh
 fi
 chmod +x /entrypoint0.sh
