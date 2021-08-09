3ff9dae27f35541dbce1e2b7f922650a83e2132e
yzqiang666 <yzqiang6@gmail.com>
2021-08-09 22:30:39 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 
 [ ! "${ENTRYPOINT_URL}" == "" ] && wget --no-check-certificate  -O /entrypoint.tmp "$ENTRYPOINT_URL"
+sleep 3
 if [ -s /entrypoint.tmp ] && [ ! "`grep \"#!/bin/bash\" /entrypoint.tmp`" == "" ] ; then
    echo "Download from url ${ENTRYPOINT_URL} file success." 
    mv /entrypoint.tmp /entrypoint0.sh 
