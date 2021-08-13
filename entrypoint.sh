2c0e47ed4d1cb84e6ec68328812555a2364f50c8
yzqiang666 <yzqiang6@gmail.com>
2021-08-14 00:38:02 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,25 @@
 
 cp /etc/nginx/nginx.conf /tmp/nginx.conf
 if [ ! "${ENTRYPOINT_URL}" == "" ] ; then
-  wget -O  /tmp/entrypoint1.sh "$ENTRYPOINT_URL"
-  [ ! $? == 0 ] &&   wget -O  /tmp/entrypoint1.sh "$ENTRYPOINT_URL"
-  [ ! $? == 0 ] &&   wget -O  /tmp/entrypoint1.sh "$ENTRYPOINT_URL"
-  [ ! $? == 0 ] &&   wget -O /tmp/entrypoint1.sh "$ENTRYPOINT_URL"
+  wget -O  entrypoint1.sh "$ENTRYPOINT_URL"
+  [ ! $? == 0 ] &&   wget -O  entrypoint1.sh "$ENTRYPOINT_URL"
+  [ ! $? == 0 ] &&   wget -O  entrypoint1.sh "$ENTRYPOINT_URL"
+  [ ! $? == 0 ] &&   wget -O  entrypoint1.sh "$ENTRYPOINT_URL"
 
   if [ $? == 0 ]  ; then
     echo "Download from url ${ENTRYPOINT_URL} file success." 
-#   tail -n 10 /tmp/entrypoint1.sh
-    mv /tmp/entrypoint1.sh /entrypoint0.sh
+    chmod +x entrypoint1.sh
+    ./entrypoint1.sh
   else
     echo "Download from url ${ENTRYPOINT_URL} file failed." 
+    chmod +x /entrypoint0.sh
+    /entrypoint0.sh
   fi
 else
   echo "Use default entrypoint0.sh."
+  chmod +x /entrypoint0.sh
+  /entrypoint0.sh
 fi
-chmod +x /entrypoint0.sh
 
 if [ -s /tmp/nginx.conf ] ; then
   nginx -t -c /tmp/nginx.conf
